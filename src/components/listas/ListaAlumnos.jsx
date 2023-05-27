966f3cdecaaaab4f0e91c1663d419deacecf71a2
Franco <francoherrera_8@hotmail.com.ar>
2023-05-27 20:46:08 -0300

    lista alumnos component

---
+++ src/components/listas/ListaAlumnos.jsx
@@ -0,0 +1,91 @@
+import React,{ useEffect, useState } from 'react';
+import {Button,Table} from 'react-bootstrap';
+import NavBar from '../commons/NavBar';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+const ListaAlumnos = () => {
+
+    const [alumnos,setAlumnos]= useState([])
+    
+    useEffect(()=>{
+     const getAlumnos = async()=>{
+        const {data} = await axios.get('https://final-react-back-production.up.railway.app/alumnos')
+        setAlumnos(data.alumno);
+       console.log(data.alumno);
+        }
+        getAlumnos();
+    },[]);
+    
+    const handleDelete = (id)=>{
+       Swal.fire({
+        title: 'Eliminar?',
+        text: "Estas Seguro de Eliminar el Archivo!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, Eliminar!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const {data} =  axios.delete("https://final-react-back-production.up.railway.app/eliminarAlumno/" + id);
+          console.log(data);
+          setAlumnos(alumnos.filter(alumno => alumno._id !== id))
+          Swal.fire(
+            'Borrado!',
+            'Alumno Eliminado con Exito.',
+            'success'
+          );
+        } 
+      });      
+    };
+  return (
+    <>
+    <NavBar />
+        <div>
+           <h1 className='text-center' style={{fontFamily:'Fredericka the Great'}}>Alumnos</h1>
+           <Table  bordered hover style={{ backgroundColor: "gainsboro",fontFamily:'Original Surfer',color:"black" }}>
+            <thead>
+            <tr>
+              <th>#</th>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Dni</th>
+              <th>Cuit</th>
+              <th>Direccion</th>
+              <th>Telefono</th>
+              <th>Curso</th>
+              <th>Año</th>
+              <th>Promedio</th>
+              <th>Descripcion</th>
+              <th className='text-center'>Editar x DNI</th>
+            </tr>
+          </thead>
+          <tbody >
+            {alumnos.map((alumno, index) => (
+              <tr key={index}>
+                <td >{index + 1}</td>
+                <td >{alumno.nombre}</td>
+                <td >{alumno.apellido}</td>
+                <td >{alumno.dni}</td>
+                <td >{alumno.cuit}</td>
+                <td >{alumno.direccion}</td>
+                <td >{alumno.telefono}</td>
+                <td >{alumno.curso}</td>
+                <td>{alumno.año}</td>
+                <td>{alumno.promedio}</td>
+                <td >{alumno.descripcion}</td>
+                <td className='text-center'>
+                  <Button  onClick={()=>handleDelete(alumno._id)}variant="outline-danger">Eliminar</Button>{" "}
+                  <Button href='/formAlumnos'variant="outline-secondary">Editar</Button>
+                </td>
+              </tr>
+            ))}</tbody>
+        </Table>
+      </div>
+      
+    </>
+  )
+};
+
+export default ListaAlumnos;
